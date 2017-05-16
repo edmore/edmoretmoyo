786148827e563ea55977e2cdd488b76bee8d59c4
Edmore Moyo <edmore@inclusivitysolutions.com>
2017-05-16 12:25:20 +0200

    Changes to CV

--- edmoretmoyo.go
+++ edmoretmoyo.go
@@ -3,13 +3,13 @@ package main
 import (
     "fmt"
     "net/http"
-    "os"
+//    "os"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("_site")))
 	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		panic(err)
 	}
